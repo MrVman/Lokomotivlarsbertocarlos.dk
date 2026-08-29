e96581a7b686f0e1089dbc6b561b99c45997ae28
MrVman <106765948+MrVman@users.noreply.github.com>
2026-08-29 09:45:35 +0200

    Tilføj ølliste-data fra PDF 2026

---
+++ js/beer.js
@@ -0,0 +1,19 @@
+window.LLC_DATA = window.LLC_DATA || {};
+window.LLC_DATA.beerList = [
+  { round: 1, weekday: "Fre", date: "2026-04-10", lars: "", home: "Lokomotiv LarsbertoCarlos", away: "Stribberne" },
+  { round: 3, weekday: "Søn", date: "2026-04-26", lars: "Verner", home: "Lokomotiv LarsbertoCarlos", away: "FC Sailing Lads" },
+  { round: 4, weekday: "Man", date: "2026-05-04", lars: "Rostrup", home: "FBK", away: "Lokomotiv LarsbertoCarlos" },
+  { round: 5, weekday: "Søn", date: "2026-05-10", lars: "Fønss", home: "Lokomotiv LarsbertoCarlos", away: "Absalons Boldklub af 2012" },
+  { round: 6, weekday: "Lør", date: "2026-05-16", lars: "Anders", home: "A-holdet", away: "Lokomotiv LarsbertoCarlos" },
+  { round: 7, weekday: "Lør", date: "2026-05-23", lars: "Biss", home: "Lokomotiv LarsbertoCarlos", away: "Valby Fulbo 1." },
+  { round: 8, weekday: "Søn", date: "2026-05-31", lars: "Zaki", home: "Svaneke Boldklub", away: "Lokomotiv LarsbertoCarlos" },
+  { round: 10, weekday: "Søn", date: "2026-06-14", lars: "Balthzer", home: "Stribberne", away: "Lokomotiv LarsbertoCarlos" },
+  { round: 11, weekday: "Fre", date: "2026-06-19", lars: "Aske", home: "Lokomotiv LarsbertoCarlos", away: "Skurvognen FC" },
+  { round: 2, weekday: "Fre", date: "2026-06-26", lars: "Rost", home: "Skurvognen FC", away: "Lokomotiv LarsbertoCarlos" },
+  { round: 12, weekday: "Søn", date: "2026-08-16", lars: "Louis", home: "FC Sailing Lads", away: "Lokomotiv LarsbertoCarlos", cancelled: true },
+  { round: 13, weekday: "Tir", date: "2026-08-25", lars: "Daae", home: "Lokomotiv LarsbertoCarlos", away: "FBK" },
+  { round: 14, weekday: "Lør", date: "2026-08-29", lars: "Zeko", home: "Absalons Boldklub af 2012", away: "Lokomotiv LarsbertoCarlos" },
+  { round: 15, weekday: "Søn", date: "2026-09-06", lars: "Magnus", home: "Lokomotiv LarsbertoCarlos", away: "A-holdet" },
+  { round: 16, weekday: "Lør", date: "2026-09-12", lars: "Jakob", home: "Valby Fulbo 1.", away: "Lokomotiv LarsbertoCarlos" },
+  { round: 17, weekday: "Lør", date: "2026-09-19", lars: "Verner", home: "Lokomotiv LarsbertoCarlos", away: "Svaneke Boldklub" }
+];
